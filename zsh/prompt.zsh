a00ca129e841241ea3c58e689bfd7be034ace5ca
Ed Jeon <muikor@gmail.com>
2020-05-28 21:53:20 +0900

    Remove useless prompt functions

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -126,7 +126,7 @@ EOF
 # ' at ' in orange outputted only if both user and host enabled
 function RPR_AT() {
     if [[ "${RPR_SHOW_USER}" == "true" ]] && [[ "${RPR_SHOW_HOST}" == "true" ]]; then
-        echo "%{$fg[blue]%} at %{$reset_color%}"
+        echo "%{$fg[blue]%}@%{$reset_color%}"
     fi
 }
 
@@ -153,67 +153,12 @@ function parse_git_branch() {
     (git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD) 2> /dev/null
 }
 
-function parse_git_detached() {
-    if ! git symbolic-ref HEAD >/dev/null 2>&1; then
-        echo "${GIT_PROMPT_DETACHED}"
-    fi
-}
-
-# Show different symbols as appropriate for various Git repository states
-function parse_git_state() {
-    # Compose this value via multiple conditional appends.
-    local GIT_STATE="" GIT_DIFF=""
-
-    local NUM_AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
-    if [ "$NUM_AHEAD" -gt 0 ]; then
-    GIT_STATE=$GIT_STATE${GIT_PROMPT_AHEAD//NUM/$NUM_AHEAD}
-    fi
-
-    local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
-    if [ "$NUM_BEHIND" -gt 0 ]; then
-        if [[ -n $GIT_STATE ]]; then
-            GIT_STATE="$GIT_STATE "
-        fi
-    GIT_STATE=$GIT_STATE${GIT_PROMPT_BEHIND//NUM/$NUM_BEHIND}
-    fi
-
-    local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
-    if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
-        if [[ -n $GIT_STATE ]]; then
-            GIT_STATE="$GIT_STATE "
-        fi
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_MERGING
-    fi
-
-    if [[ -n $(git ls-files --other --exclude-standard :/ 2> /dev/null) ]]; then
-    GIT_DIFF=$GIT_PROMPT_UNTRACKED
-    fi
-
-    if ! git diff --quiet 2> /dev/null; then
-    GIT_DIFF=$GIT_DIFF$GIT_PROMPT_MODIFIED
-    fi
-
-    if ! git diff --cached --quiet 2> /dev/null; then
-    GIT_DIFF=$GIT_DIFF$GIT_PROMPT_STAGED
-    fi
-
-    if [[ -n $GIT_STATE && -n $GIT_DIFF ]]; then
-        GIT_STATE="$GIT_STATE "
-    fi
-    GIT_STATE="$GIT_STATE$GIT_DIFF"
-
-    if [[ -n $GIT_STATE ]]; then
-    echo "$GIT_PROMPT_PREFIX$GIT_STATE$GIT_PROMPT_SUFFIX"
-    fi
-}
-
 # If inside a Git repository, print its branch and state
 RPR_SHOW_GIT=true # Set to false to disable git status in rhs prompt
 function git_prompt_string() {
     if [[ "${RPR_SHOW_GIT}" == "true" ]]; then
         local git_where="$(parse_git_branch)"
-        local git_detached="$(parse_git_detached)"
-        [ -n "$git_where" ] && echo " $GIT_PROMPT_SYMBOL$(parse_git_state)$GIT_PROMPT_PREFIX%{$fg[magenta]%}%B${git_where#(refs/heads/|tags/)}%b$git_detached$GIT_PROMPT_SUFFIX"
+        [ -n "$git_where" ] && echo " $GIT_PROMPT_SYMBOL$GIT_PROMPT_PREFIX%{$fg[magenta]%}%B${git_where#(refs/heads/|tags/)}%b$GIT_PROMPT_SUFFIX"
     fi
 }
 
@@ -229,82 +174,10 @@ function PR_EXTRA() {
     # do nothing by default
 }
 
-# Show select exported environment variables
-
-_pr_var_list=()
-_vars_multiline=false
-
-function vshow() {
-    local v
-    for v in "$@"; do
-        if [[ "${v}" =~ '[A-Z_]+' ]]; then
-            if [[ ${_pr_var_list[(i)${v}]} -gt ${#_pr_var_list} ]]; then
-                _pr_var_list+=("${v}")
-            fi
-        fi
-    done
-}
-
-function vhide() {
-    local v
-    for v in "$@"; do
-        _pr_var_list[${_pr_var_list[(i)${v}]}]=()
-    done
-}
-
-function vmultiline() {
-    if $_vars_multiline; then
-        _vars_multiline=false
-    else
-        _vars_multiline=true
-    fi
-}
-
-function PR_VARS() {
-    local i v spc nl
-    if $_vars_multiline; then
-        spc=""
-        nl="\n"
-    else
-        spc=" "
-        nl=""
-    fi
-    for ((i=1; i <= ${#_pr_var_list}; i++)) do
-        local v=${_pr_var_list[i]}
-        if [[ -v "${v}" ]]; then
-            # if variable is set
-            if export | grep -Eq "^${v}="; then
-                # if exported, show regularly
-                printf '%s' "$spc%{$fg[cyan]%}${v}=${(P)${v}}%{$reset_color%}$nl"
-            else
-                # if not exported, show in red
-                printf '%s' "$spc%{$fg[red]%}${v}=${(P)${v}}%{$reset_color%}$nl"
-            fi
-        fi
-    done
-    # show project-specific vars
-    while read v; do
-        if [[ "${v}" =~ '[A-Z_]+' ]]; then
-            # valid environment variable
-            if [[ ${_pr_var_list[(i)${v}]} -gt ${#_pr_var_list} ]]; then
-                # not shown yet
-                if export | grep -Eq "^${v}="; then
-                    # exported
-                    printf '%s' "$spc%{$fg[cyan]%}${v}=${(P)${v}}%{$reset_color%}$nl"
-                fi
-            fi
-        fi
-    done < <(git exec cat .showvars 2>/dev/null)
-}
-
 # Prompt
 function PCMD() {
     if (( PROMPT_MODE == 0 )); then
-        if $_vars_multiline; then
-            echo "$(PR_VARS)$(PR_EXTRA)$(PR_DIR) $(PR_ERROR)$(PR_ARROW) " # space at the end
-        else
-            echo "$(PR_EXTRA)$(PR_DIR)$(PR_VARS) $(PR_ERROR)$(PR_ARROW) " # space at the end
-        fi
+        echo "$(PR_EXTRA)$(PR_DIR) $(PR_ERROR)$(PR_ARROW) " # space at the end
     elif (( PROMPT_MODE == 1 )); then
         echo "$(PR_EXTRA)$(PR_DIR 1) $(PR_ERROR)$(PR_ARROW) " # space at the end
     else
