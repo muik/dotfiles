168120cb9e67e6c180e43b719dd1d714243f5ed9
Ed Jeon <ed@daangn.com>
2020-06-02 21:20:33 +0900

    feat: Add ls color on mac

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,5 +1,9 @@
 # Use colors in coreutils utilities output
-#alias ls='ls --color=auto'
+if [[ `uname` == "Darwin" ]]; then
+    alias ls='ls -G'
+else
+    alias ls='ls --color=auto'
+fi
 alias grep='grep --color'
 
 # ls aliases
